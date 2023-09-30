7f31115268d6015ba6f66564cc1f2ff2a924ac79
HemantDutta <hemantnorddutta2@gmail.com>
2023-09-30 09:59:22 +0530

    Create Node Express Server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from 'cors';
+
+//Port Number
+const port = 5000;
+
+//Express App
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+//Index Page Listener
+app.get("/", (req,res)=>{
+   res.send(`<h1>Brand Mirror Server</h1>`);
+});
+
+
+//Server Listener
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${5000}`);
+});
