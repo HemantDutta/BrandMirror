c0ba80d9ebc1061e1a109b28c39eaff7016cbb43
HemantDutta <hemantnorddutta2@gmail.com>
2023-11-25 01:47:40 +0530

    Make Hero Responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -23,24 +23,24 @@ export const Home = () => {
                 </section>
                 {/*  Hero End  */}
                 {/*  About  */}
-                <section className="about" id="about">
-                    <div className="about-container">
-                        <div className="header">
-                            <span className="gradient-hover">Who are we?</span>
-                        </div>
-                        <div className="about-grid">
-                            <span className="about-item text">We, the founders of BrandMirror, are <span className="cali gradient-text">three friends</span> driven by a shared passion to revolutionize brand analysis. United in our dedication, we bring diverse skills to empower brands with advanced web scraping and NLP technologies.</span>
-                            <div className="about-item img">
-                                <img src="images/BM_ab1.webp" alt="Hello!"/>
-                            </div>
-                            <span className="about-item text">BrandMirror? It all started in an NLP lecture. One founder - tech news - web scraping. Boom! Idea: let's decode <span className="cali gradient-text">brands' digital vibes</span>. And just like that, BrandMirror was born – where NLP and web scraping make brand magic happen! </span>
-                            <div className="about-item img">
-                                <img src="images/BM_ab2.jpg" alt="How are you doing?"/>
-                            </div>
-                            <span className="about-item text">Our mission? To provide an <span className="cali gradient-text">affordable alternative</span>, steering clear of expensive software, utilizing the prowess of NLP and web scraping to make brand analysis seamless.</span>
-                        </div>
-                    </div>
-                </section>
+                {/*<section className="about" id="about">*/}
+                {/*    <div className="about-container">*/}
+                {/*        <div className="header">*/}
+                {/*            <span className="gradient-hover">Who are we?</span>*/}
+                {/*        </div>*/}
+                {/*        <div className="about-grid">*/}
+                {/*            <span className="about-item text">We, the founders of BrandMirror, are <span className="cali gradient-text">three friends</span> driven by a shared passion to revolutionize brand analysis. United in our dedication, we bring diverse skills to empower brands with advanced web scraping and NLP technologies.</span>*/}
+                {/*            <div className="about-item img">*/}
+                {/*                <img src="images/BM_ab1.webp" alt="Hello!"/>*/}
+                {/*            </div>*/}
+                {/*            <span className="about-item text">BrandMirror? It all started in an NLP lecture. One founder - tech news - web scraping. Boom! Idea: let's decode <span className="cali gradient-text">brands' digital vibes</span>. And just like that, BrandMirror was born – where NLP and web scraping make brand magic happen! </span>*/}
+                {/*            <div className="about-item img">*/}
+                {/*                <img src="images/BM_ab2.jpg" alt="How are you doing?"/>*/}
+                {/*            </div>*/}
+                {/*            <span className="about-item text">Our mission? To provide an <span className="cali gradient-text">affordable alternative</span>, steering clear of expensive software, utilizing the prowess of NLP and web scraping to make brand analysis seamless.</span>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</section>*/}
                 {/*  About End  */}
             </div>
             {/*  Body End  */}
