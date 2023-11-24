31ff18fbf83c0ce887080f08c3dbe28e832d5084
HemantDutta <hemantnorddutta2@gmail.com>
2023-11-25 00:06:46 +0530

    Create Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,56 @@
+import {Link} from "react-router-dom";
+
 export const Navbar = () => {
+
+    //Nav Menu Toggle
+    function navMenuToggle() {
+        let menu = document.getElementById("menu");
+        let ham = document.getElementById("ham");
+
+        if (menu.classList.contains("open")) {
+            ham.classList.remove("fa-xmark");
+            ham.classList.add("fa-bars");
+            menu.classList.remove("open");
+            setTimeout(() => {
+                menu.style.display = "none";
+            }, 400)
+        } else {
+            menu.style.display = "flex";
+            ham.classList.remove("fa-bars");
+            ham.classList.add("fa-xmark");
+            setTimeout(()=>{
+                menu.classList.add("open");
+            },50)
+        }
+    }
+
     return (
         <>
-            Navbar
+            <nav>
+                <div className="navbar-container">
+                    <div className="navbar-left">
+                        <span className="gradient-text">BrandMirror</span>
+                    </div>
+                    <div className="navbar-right">
+                        <div className="contact">
+                            <div className="text">
+                                <span>Contact</span>
+                                <span className="gradient-text">Contact</span>
+                            </div>
+                            <i className="fa-solid fa-arrow-right"/>
+                        </div>
+                        <div className="ham-menu">
+                            <i className="fa-solid fa-bars" id="ham" onClick={navMenuToggle}/>
+                        </div>
+                        <div className="menu" id="menu">
+                            <Link to={"/"}>Join Now&nbsp;<i className="fa-solid fa-arrow-right"/></Link>
+                            <Link to={"/"}>Home&nbsp;<i className="fa-solid fa-arrow-right"/></Link>
+                            <Link to={"/"}>About&nbsp;<i className="fa-solid fa-arrow-right"/></Link>
+                            <Link to={"/"}>Contact Us&nbsp;<i className="fa-solid fa-arrow-right"/></Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
